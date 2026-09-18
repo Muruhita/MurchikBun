38c27318984c1054ea9130eb23fdd7718aa08de0
Mur <195444383+Muruhita@users.noreply.github.com>
2026-09-19 00:43:42 +0700

    Update geh.js

--- pages/geh.js
+++ pages/geh.js
@@ -9,7 +9,7 @@ export default function Geh() {
       <div className="legal-page">
         <div className="legal-container">
           <h1>🔒 Политика Конфиденциальности</h1>
-          <p className="updated">Последнее обновление: сентябрь 2026</p>
+          <p className="updated">Последнее обновление:22:22 18/09/2026</p>
 
           <section>
             <h2>1. Общее</h2>
