fe0e1ceb1f1227d6692a3399035a894ca7c17a2a
Mur <195444383+Muruhita@users.noreply.github.com>
2026-09-19 16:20:55 +0700

    Update author.js

--- pages/author.js
+++ pages/author.js
@@ -7,7 +7,7 @@ const AUTHOR = {
   displayName: 'Mura Kiratu',
   discordId: '1018113109346504744',
   email: 'murkilanki@gmail.com',
-  avatar: 'https://cdn.discordapp.com/avatars/1018113109346504744/0b9b2f95e6a2b4b8a1b5a2b0b2b2b2b2.png?size=512',
+  avatar: 'https://i.pinimg.com/736x/57/48/10/5748107f528598de8e909b60ff7324aa.jpg',
   roles: ['Админ', 'Автор сия бота'],
   bio: 'Создатель и хранитель FIB Forms.'
 };
